de61cc645b72e782070c13df10fa71d08a5be8f6
Erika <erika@beasleys.com>
2020-06-30 21:30:54 -0400

    Updating files

---
+++ script.js
@@ -0,0 +1,134 @@
+//Declare Variables/////
+var input;
+var confirmLowerCase;
+var confirmUpperCase;
+var confirmNumber;
+var confirmSpecialCharacter;
+var choices;
+
+lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+number = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+// Write password to the #password input
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+
+    passwordText.value = password;
+}
+
+//Function to generate Password//////
+function generatePassword() {
+    //ask for user input
+    input = parseInt(promt("How many characters would you like in your password? (Must be at least 8 and no more than 128"));
+    //if invlaid
+    if (!input) {
+        alert("You must have a value!");
+    }else if (input < 8 || input > 128) {
+        alert("Your password must choose between 8 and 128 characters");
+    }
+    else {
+        confirmLowerCase = confirm("Would you like to include uppercase letters?");
+        confirmUpperCase = confirm("Would you like to include lowercase letters?");
+        confirmNumber = confirm("Would you like to include numbers?");
+        confirmSpecialCharacter = confirm("Would you like to include special characters?");
+    };
+
+    //If no criteria is selected
+    if (!confirmLowerCase && !confirmUpperCase && !confirmNumber && !confirmSpecialCharacter) {
+        choices = alert("You must select criteria for your password.");
+    }
+    //Else if all four selected yes
+    else if (confirmLowerCase && confirmUpperCase && confirmNumber) {
+        choices = character.concat(lower,upper,number);
+    }
+
+    //Else if three selections
+    else if (confirmLowerCase && confirmUpperCase && confirmSpecialCharacter) {
+        choices = lower.concat(upper,special);
+    }
+    else if (confirmLowerCase && confirmNumber && confirmSpecialCharacter) {
+        choices = lower.concat(number,special);
+    }
+    else if (confirmUpperCase && confirmNumber && confirmSpecialCharacter) {
+        choices = upper.concat(number,special);
+    }
+
+    //Else if two selections
+    else if (confirmLowerCase && confirmUpperCase) {
+        choices = lower.concat(upper);
+    }
+    else if (confirmLowerCase && confirmNumber) {
+        choices = lower.concat(number);
+    }
+    else if (confirmLowerCase && confirmSpecialCharacter) {
+        choices = lower.concat(special);
+    }
+    else if (confirmUpperCase && confirmNumber) {
+        choices = upper.concat(number);
+    }
+    else if (confirmUpperCase && confirmSpecialCharacter) {
+        choices = upper.concat(special);
+    }
+    else if (confirmNumber && confirmSpecialCharacter) {
+        choices = number.concat(special);
+    }
+
+    //Else if one selection
+    else if (confirmLowerCase) {
+        choices = lower;
+    }
+    else if (confirmUpperCase) {
+        choices = upper;
+    }
+    else if (confirmNumber) {
+        choices = number;
+    }
+    else if (confirmSpecialCharacter) {
+        choices = special;
+    }
+
+    var password = [];
+
+    //Random Selection for variables
+    // Random Function
+    // var randomFunction = {
+    //     lower: getRandomLowerCase,
+    //     upper: getRandomUpperCase,
+    //     number: getRandomNumber,
+    //     special: getRandomSpecialCharacter
+    // };
+
+    // //Generator functions
+    // function getRandomLowerCase() {
+    //     return String.fromCharCode(Math.floor(Math.random() *26) + 97);
+    // }
+    // function getRandomUpperCase() {
+    //     return String.fromCharCode(Math.floor(Math.random() *26) + 65);
+    // }
+    // function getRandomNumber() {
+    //     return String.fromCharCode(Math.floor(Math.random() *10) + 48);
+    // }
+    // function getRandomSpecialCharacter() {
+    //     var specialCharacter = "!@#$%^&*(){}[]+<>/,.'"
+    //     return specialCharacter[Math.floor(Math.random() *specialCharacter.length)];
+    // }
+
+    for (var i = 0; i < input; i++) {
+        var pickChoices = choices[Math.floor(Math.random() * choices.length)];
+        password.push(pickChoices);
+    }
+
+    
+
+
+}
+//Put
